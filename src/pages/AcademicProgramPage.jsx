5f159fe248883ec921745ea7beae98b8ef92ca49
Mohand Abdelkader <mohand27m@gmail.com>
2025-05-26 16:59:49 +0300

    academic program page

--- src/pages/AcademicProgramPage.jsx
+++ src/pages/AcademicProgramPage.jsx
@@ -1,5 +1,20 @@
+import { Button } from "../ui/Button";
+import AcademicHero from "../components/AcademicHero";
+import ProgramsOverview from "../components/ProgramsOverview";
+import CallToAction from "../components/CallToAction";
+import Curriculum from "../components/Curriculum";
 function AcademicProgramPage() {
-  return <div className="h-screen mt-[20rem] text-center">Academic Program Page</div>;
+  return (
+    <div className="pt-16">
+      <AcademicHero />
+
+      <ProgramsOverview />
+
+      <Curriculum />
+
+      <CallToAction />
+    </div>
+  );
 }
 
 export default AcademicProgramPage;
