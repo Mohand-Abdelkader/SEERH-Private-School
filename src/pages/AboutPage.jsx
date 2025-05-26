3624c5a6c9fcdcfa75922f058a39039795013e48
Mohand Abdelkader <mohand27m@gmail.com>
2025-05-26 16:46:08 +0300

    about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,23 @@
+import { Button } from "../ui/Button";
+import AboutHero from "../components/AboutHero";
+import MissionVision from "../components/MissionVision";
+import History from "../components/History";
+import Values from "../components/Values";
+import CallToAction from "../components/CallToAction";
 function AboutPage() {
-  return <div  className="h-screen mt-[20rem] text-center">About Page</div>;
+  return (
+    <div className="pt-16">
+      <AboutHero />
+
+      <MissionVision />
+
+      <History />
+
+      <Values />
+
+      <CallToAction />
+    </div>
+  );
 }
 
 export default AboutPage;
